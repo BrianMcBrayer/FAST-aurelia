60b0d62f7ccea836057069658e595a0b09814a87
BrianMcBrayer <brandmcbrayer@live.com>
2015-03-07 10:33:41 -0500

    Added 500 and other routes

--- src/app.js
+++ src/app.js
@@ -9,10 +9,11 @@ export class App {
       config.map([
         { route: ['', 'home'], moduleId: 'home', nav: true, title: 'Home'},
         { route: ['multibind'], moduleId: 'multibind', nav: true, title: 'Multibind'},
-        { route: ['multibind/multibind100'], moduleId: 'multibind100', nav: true, title: 'Multibind100'}
-        //{ route: ['multibind/multibind1000'], moduleId: 'multibind1000', nav: true, title: 'Multibind1000'},
-        //{ route: ['multibind/multibind5000'], moduleId: 'multibind5000', nav: true, title: 'Multibind5000'},
-        //{ route: ['multibind/multibind10000'], moduleId: 'multibind10000', nav: true, title: 'Multibind10000'},
+        { route: ['multibind/multibind100'], moduleId: 'multibind100', nav: true, title: 'Multibind100'},
+        { route: ['multibind/multibind500'], moduleId: 'multibind500', nav: true, title: 'Multibind500'},
+        { route: ['multibind/multibind1000'], moduleId: 'multibind1000', nav: true, title: 'Multibind1000'},
+        { route: ['multibind/multibind5000'], moduleId: 'multibind5000', nav: true, title: 'Multibind5000'},
+        { route: ['multibind/multibind10000'], moduleId: 'multibind10000', nav: true, title: 'Multibind10000'},
       ]);
     });
   }
